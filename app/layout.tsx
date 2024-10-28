bdc8894f467e68948159cc9bdbe967745092f9b2
prashant <prashantchouhan711@gmail.com>
2024-10-28 13:07:08 +0530

    implement initial file and folder structure

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import {Inter ,IBM_Plex_Serif} from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({subsets:["latin"], variable:"--font-inter"}); 
+const IbmPlexSerif = IBM_Plex_Serif({subsets:["latin"],weight:['400','700'], variable:"--font-ibm-plex-serif"}); 
+
+export const metadata: Metadata = {
+  title: "NexPay",
+  description: "modern world banking app",
+  icons:{
+    icon: '/icons/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} ${IbmPlexSerif.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
